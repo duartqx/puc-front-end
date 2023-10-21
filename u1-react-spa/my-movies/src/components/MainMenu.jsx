f58666c9c266a2f5dc7656190f4defedae610dec
Diego Duarte <duartqx@gmail.com>
2023-10-20 21:50:00 -0300

    Link to (react-router-dom)

--- u1-react-spa/my-movies/src/components/MainMenu.jsx
+++ u1-react-spa/my-movies/src/components/MainMenu.jsx
@@ -1,11 +1,16 @@
-import { MainMenuContainer } from "../styles/MainMenuContainer"
+import { Link } from "react-router-dom";
+import { MainMenuContainer } from "../styles/MainMenuContainer";
 
 export default function MainMenu() {
   return (
     <MainMenuContainer>
       <ul>
-        <li>Home</li>
-        <li>Sobre</li>
+        <li>
+          <Link to="">Home</Link>
+        </li>
+        <li>
+          <Link to="about">Sobre</Link>
+        </li>
       </ul>
     </MainMenuContainer>
   );
